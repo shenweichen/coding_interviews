9ab1ac3d964169f0d20ba46a57ebf73d20e7268d
shenweichen <wcshen1994@163.com>
2020-01-07 00:18:26 +0800

    no message

--- 10.变态跳台阶/10.变态跳台阶(DP).py
+++ 10.变态跳台阶/10.变态跳台阶(DP).py
@@ -2,4 +2,8 @@
 class Solution:
     def jumpFloorII(self, number):
         # write code here
-        return 1<<(number-1)
+        dp = [0,1,2]
+        for i in range(3,number+1):
+            dp.append(sum(dp)+1)
+        return dp[number]
+            
--- 10.变态跳台阶/10.变态跳台阶(Math).py
+++ 10.变态跳台阶/10.变态跳台阶(Math).py
@@ -2,8 +2,4 @@
 class Solution:
     def jumpFloorII(self, number):
         # write code here
-        dp = [0,1,2]
-        for i in range(3,number+1):
-            dp.append(sum(dp)+1)
-        return dp[number]
-            
+        return 1<<(number-1)
